63109e339ea9970a56874087699586608e55ac82
linhhoang060408-cpu <linhhoang060408@gmail.com>
2026-06-03 04:00:47 +0700

    Update route.ts

--- app/api/messgaes/[id]/reaction/route.ts
+++ app/api/messgaes/[id]/reaction/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import prisma from '@/lib/prisma'
+import { prisma } from '../../../../../lib/prisma';
 export async function PATCH(request: Request, { params }: { params: { id: string } }) {
   try {
     const body = await request.json();
